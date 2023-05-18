7f6ef56ebda63e409834a9304aeed378890bd3fb
nointh <ngthnoi00@gmail.com>
2023-05-18 07:34:20 +0700

    Add mongo library

--- model_api/src/main.py
+++ model_api/src/main.py
@@ -1,10 +1,30 @@
+import os
 from fastapi import FastAPI
 from routers import prediction
+from pymongo import MongoClient
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+MONGO_USERNAME = os.environ.get("MONGO_USERNAME")
+MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD")
+
+mongo_uri = f'mongodb+srv://MONGO_USERNAME:MONGO_PASSWORD@cluster0.7julke9.mongodb.net/?retryWrites=true&w=majority'
 
 app = FastAPI(title="Multivariate time series forecasting model serving API")
 
 app.include_router(prediction.router)
 
+@app.on_event("startup")
+def start_db_client():
+    app.mongo_client = MongoClient(mongo_uri)
+    app.database = app.mongo_client['stock_price']
+    print(app.database)
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongo_client.close()
+
 @app.get('/')
 async def root():
-    return {"message": "Welcome to Model Serving API"}
+    return {"message": "Welcome to Model Serving API for Multivariate Time Series Forecasting"}
