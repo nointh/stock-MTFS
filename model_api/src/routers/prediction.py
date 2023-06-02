d0651333cd1c857aed01ce4cdf8d957e4c6327e7
vqh164 <vqh.official@gmail.com>
2023-06-03 00:34:30 +0700

    update metric

--- model_api/src/routers/prediction.py
+++ model_api/src/routers/prediction.py
@@ -16,7 +16,6 @@ def fetch_records(request, algorithm, pred_len):
     # Access the 'prediction' collection
     collection = database['prediction']
     # Calculate the start and end dates
-    # start_date = datetime.now().date() + timedelta(days=1)
     start_date = datetime.now().date()
     end_date = start_date + timedelta(days=pred_len)
 
@@ -34,19 +33,23 @@ def fetch_records(request, algorithm, pred_len):
             "close": record["close"]
         })
 
+    # Fetch the prediction metric from the 'predictionMetric' collection
+    prediction_metric_collection = database['predictionMetric']
+    prediction_metric = prediction_metric_collection.find_one({"algorithm": algorithm})
+
+    if prediction_metric:
+        # Include the prediction metric in the response
+        data["predictionMetric"] = {
+            "algorithm": prediction_metric.get("algorithm", ""),
+            "ape": prediction_metric.get("ape", 0),
+            "me": prediction_metric.get("me", 0),
+            "mae": prediction_metric.get("mae", 0),
+            "mpe": prediction_metric.get("mpe", 0),
+            "rmse": prediction_metric.get("rmse", 0),
+            "corr": prediction_metric.get("corr", 0),
+            "minmax": prediction_metric.get("minmax", 0),
+        }
     return data
-
-
-@router.get('/vn30/long-term')
-def get_long_term_multistock_prediction(request: Request, pred_len: int = 50):
-    try:
-        # Fetch records for 'long_term' algorithm
-        return fetch_records(request, "long_term", pred_len)
-
-    except Exception as e:
-        # Handle the exception and return an error response
-        error_message = {"error": str(e)}
-        return error_message
 
 
 @router.get('/lstnet')
@@ -54,6 +57,18 @@ def get_lstnet_multistock_prediction(request: Request, pred_len: int = 50):
     try:
         # Fetch records for 'lstnet' algorithm
         return fetch_records(request, "lstnet", pred_len)
+
+    except Exception as e:
+        # Handle the exception and return an error response
+        error_message = {"error": str(e)}
+        return error_message
+
+
+@router.get('/vn30/long-term')
+def get_long_term_multistock_prediction(request: Request, pred_len: int = 50):
+    try:
+        # Fetch records for 'long_term' algorithm
+        return fetch_records(request, "long_term", pred_len)
 
     except Exception as e:
         # Handle the exception and return an error response
