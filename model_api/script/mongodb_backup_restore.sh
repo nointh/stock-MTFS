f8c07775cf0e2152096c3d57e4febf0404cebfac
vqh164 <vqh.official@gmail.com>
2023-07-03 23:35:11 +0700

    backup database

--- model_api/script/mongodb_backup_restore.sh
+++ model_api/script/mongodb_backup_restore.sh
@@ -2,5 +2,6 @@
 
 current_location=$(pwd)
 
-mongorestore --uri "mongodb+srv://user_mongodb_api_sync:user_mongodb_api_sync@cluster0.uwsidsd.mongodb.net" --writeConcern '{w: "majority"}' "$current_location" &&
 mongodump --uri "mongodb+srv://user_mongodb_model:user_mongodb_model@cluster0.7julke9.mongodb.net/?retryWrites=true&w=majority/stock_price" --out "$current_location"
+
+# mongorestore --uri "mongodb+srv://user_mongodb_api_sync:user_mongodb_api_sync@cluster0.uwsidsd.mongodb.net" --writeConcern '{w: "majority"}' "$current_location" 
